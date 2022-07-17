45863a49a5e9670c013104481d89d04a60bf8fde
namratawadekar42 <101054924+namratawadekar42@users.noreply.github.com>
2022-07-17 20:35:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib 
 model=joblib.load('uber')
 st.title("UBER:NO OF WEEKLY RIDERS")
-f=st.number_input("enter price per week",15.00,110.00,step=5)
+f=st.number_input("enter price per week",15.00,110.00,step=5.00)
 s=st.number_input("enter population",1800000,1600000,step=10000)
 t=st.number_input("monthly income",16200,5800,step=100)
 ft=st.number_input("average parking per month",50,200,step=5)
