9444a5ef59143d2323e14f7e4a917a07227b2505
namratawadekar42 <101054924+namratawadekar42@users.noreply.github.com>
2022-07-17 19:56:29 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[server]\n\
+headless= true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
