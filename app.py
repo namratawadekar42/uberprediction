697ae545039c8da2997a273f46fab1bd8836171f
namratawadekar42 <101054924+namratawadekar42@users.noreply.github.com>
2022-07-17 20:53:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import joblib 
 model=joblib.load('uber')
-st.title("UBER:NO OF WEEKLY RIDERS")
+st.title("UBER:NO OF WEEKLY RIDES")
 f=st.number_input("enter price per week",15.00,110.00,step=5.00)
 s=st.number_input("enter population",1600000,1800000,step=10000)
 t=st.number_input("monthly income",5800,16200,step=100)
 ft=st.number_input("average parking per month",50,200,step=5)
 op=model.predict([[f,s,t,ft]])
 if st.button('PREDICT'):
-  st.title(op[0])
+  st.title("NO of weekly rides",op[0])
     
